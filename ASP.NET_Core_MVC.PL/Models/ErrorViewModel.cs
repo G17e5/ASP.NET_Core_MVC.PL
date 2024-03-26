2d69eef4fb810f16e6ea531981febcbfeff7c340
G17e5 <geogamilrge23@gmail.com>
2024-03-26 08:57:32 +0200

    Add project files.

---
+++ ASP.NET_Core_MVC.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_Core_MVC.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
