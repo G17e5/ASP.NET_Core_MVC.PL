a5a34afe4fc981ba11f92385b9139cfcb9556369
G17e5 <geogamilrge23@gmail.com>
2024-03-27 02:10:17 +0200

    Part 08 3-Layers Arch. Pattern - Business Logic Layer

---
+++ ASP.NET_Core_MVC.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using ASP.NET_Core_MVC.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+        Department Get(int id);
+        int Add(Department entity);
+        int Update(Department entity);
+        int Delete(Department entity);
+    }
+}
---
+++ ASP.NET_Core_MVC.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,51 @@
+﻿using ASP.NET_Core_MVC.BLL.Interfaces;
+using ASP.NET_Core_MVC.DAL.Data;
+using ASP.NET_Core_MVC.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC.BLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {   //Object member 
+        private readonly ApplicationDbContext _dbContext;
+
+        public DepartmentRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int Add(Department entity)
+        {
+            _dbContext.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+        public int Update(Department entity)
+        {
+            _dbContext.Departments.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+        public int Delete(Department entity)
+        {
+            _dbContext.Departments.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+        public Department Get(int id)
+        {
+            //Part 08 27:00
+            //Search Local First then Search DB 
+            // return _dbContext.Departments.Find(10);
+            return _dbContext.Find<Department>(id);//EF Core 3.1 NEW Feature
+        }
+        public IEnumerable<Department> GetAll()
+        {
+            return _dbContext.Departments.AsNoTracking().ToList();
+        }
+
+
+    }
+}
